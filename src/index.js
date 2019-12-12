fd7e8154553fb7f2ad2347fdb628b6692d594f4c
Tom P <tom@aftertheflood.com>
2019-12-12 15:44:35 +0000

    add basic swipe listening

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 const prediction = require('./generator');
 
+let pullDeltaX = 0;
+let deg = 0;
+let decisionThreshold = 80;
+let swiping = true;
+const dragOrigin = [];
+
+
 const newSeed = ()=>{
     const seed = Math.floor(Math.random()*1000000000000);
     const urlParams = new URLSearchParams(window.location.search);
@@ -16,13 +23,60 @@ const newPrediction = (seed)=>{
     document.querySelector('#prediction-fragment').innerHTML = result.sentence;
     document.querySelector('#seed-link').innerHTML = seed;
     document.querySelector('.tweet-link').href = `https://twitter.com/share?text=Prediction: ${result.prediction}&url=${window.location}&hashtags=2020,predictions,thoughtleadership,${tagify(result.mrkt)},${tagify(result.noun)}`;
+
+}
+
+function startSwipe(e){
+    dragOrigin[0] = e.pageX || e.originalEvent.touches[0].pageX;
+    dragOrigin[1] = e.pageY || e.originalEvent.touches[0].pageY;
+    swiping = true;
+    console.log('start', this);
+}
+
+function stopSwipe(e){
+    swiping = true;
+    console.log('stop', this);
+}
+
+function move(e){
+    if(!swiping) return;
+    var x = e.pageX || e.originalEvent.touches[0].pageX;
+    pullDeltaX = (x - dragOrigin[0]);
+    if (!pullDeltaX) return;
+    pullChange(document.querySelector('.card.active'));
+}
+
+function discard(activeCard){
+    activeCard.classList.add('inactive');
+    activeCard.classList.remove('active');
+    if (pullDeltaX >= decisionThreshold) {
+        activeCard.classList.add('to-right');
+    } else if (pullDeltaX <= -decisionThreshold) {
+        activeCard.classList.add('to-left');
+    }
+    // set new active card
+}
+
+function pullChange(activeCard) {
+    console.log(activeCard, pullDeltaX);
+    activeCard.style.transform = `translateX(${pullDeltaX}px) rotate(${pullDeltaX/10}deg)`;
+    if(Math.abs(pullDeltaX)>decisionThreshold){
+        discard(activeCard)
+    }
+}
+
+const swipeable = ()=>{
+    document.querySelector('.card.active').addEventListener('mousedown', startSwipe);
+    document.querySelector('.card.active').addEventListener('mouseup', stopSwipe);
+    document.querySelector('.card.active').addEventListener('mouseout', stopSwipe);
+    document.addEventListener('mousemove', move)
 }
 
 window.onload = ()=>{
     const urlParams = new URLSearchParams(window.location.search);
     const querySeed = urlParams.get('seed');
     const seed = querySeed ? querySeed : newSeed();
-
+    swipeable();
     newPrediction(seed);
     document.querySelector('#refresh').onclick = (e)=>{
         console.log('refresh')
